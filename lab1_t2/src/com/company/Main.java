02cae10d05791c215b11a2c08970672fdaa31fdc
mrk1379 <58387945+mrk1379@users.noreply.github.com>
2020-10-06 20:54:14 +0330

    lab session 1 t2

---
+++ lab1_t2/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int zarb1=1;zarb1<11;zarb1++){
+            for (int zarb2=1;zarb2<11;zarb2++){
+                System.out.print(zarb1*zarb2+ "\t");
+                
+            }
+            System.out.println();
+        }
+
+    }
+}
